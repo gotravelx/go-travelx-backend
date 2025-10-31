27e377ee2a350cbd8b8438ad6334289413ffaa45
devendraInfograins <devendralowanshi@infograins.com>
2025-10-31 11:35:19 +0530

    checking server

--- routes/FlightEventRoutes.js
+++ routes/FlightEventRoutes.js
@@ -8,16 +8,16 @@ import { validateToken } from "../middleware/validateToken.js";
 const router = express.Router();
 
 /* ================== table route's start =========================*/ 
-router.get("/get-flight-status/:flightNumber",validateToken, fetchFlightDetails)
-router.post("/decrypt-flight-data",validateToken, decryptFlightData);
-router.get("/fetch-historical/:flightNumber/date-range",validateToken, getHistoricalData);
+router.get("/get-flight-status/:flightNumber", fetchFlightDetails)
+router.post("/decrypt-flight-data", decryptFlightData);
+router.get("/fetch-historical/:flightNumber/date-range", getHistoricalData);
 /* ================== table route's end =========================*/ 
 
 router.get("/get-all-flights",validateToken, getAllFlightDetails);
 
 
 /* ================== table route's start =========================*/ 
-router.get("/create-table",validateToken, createFlightEventTable);
-router.get("/clear-table",validateToken, clearFlightEventTableData);
+router.get("/create-table", createFlightEventTable);
+router.get("/clear-table", clearFlightEventTableData);
 /* ================== table route's end   =========================*/
 export default router;
